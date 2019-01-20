969fd3dd11704238db8fd45308124b164fda6af1
lisider <1368317399@qq.com>
2019-01-20 15:29:19 +0800

    添加 audio,gstreamer lib

---
+++ audio/gstreamer/test1.c
@@ -0,0 +1,35 @@
+/*************************************************************************
+  > File Name: test.c
+  > Author: Sues
+  > Mail: sumory.kaka@foxmail.com 
+  > Created Time: 2018年03月08日 星期四 19时13分36秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdio.h>
+#include <gst/gst.h>
+
+    int
+main (int   argc,
+        char *argv[])
+{
+    const gchar *nano_str;
+    guint major, minor, micro, nano;
+
+    gst_init (&argc, &argv);
+
+    gst_version (&major, &minor, &micro, &nano);
+
+    if (nano == 1)
+        nano_str = "(CVS)";
+    else if (nano == 2)
+        nano_str = "(Prerelease)";
+    else
+        nano_str = "";
+
+    printf ("This program is linked against GStreamer %d.%d.%d %s\n",
+            major, minor, micro, nano_str);
+
+    return 0;
+
+}
